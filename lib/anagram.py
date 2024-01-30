4525e6597dac166ed7843e34cbd58b9ad9f80e99
BenjaminJacobson <ben.jacobson@flatironschool.com>
2024-01-29 22:02:27 -0500

    completed lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+        # sorted(str1) == sorted(str2)
+
+    def match(self, list_of_words):
+        matches =[]
+        for i in list_of_words:
+            if sorted(i) == sorted(self.word):
+                matches.append(i)
+        return matches
